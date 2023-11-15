893473dca68ffc0c6f92f687b2c9068720deb197
marsdigi <81784993+marsdigi@users.noreply.github.com>
2023-11-15 14:40:35 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,20 @@
+# app.py
+
+import streamlit as st
+
+def main():
+    st.title("My Streamlit App")
+    
+    # Add some text to the app
+    st.write("Welcome to my Streamlit app!")
+
+    # Add a simple input widget
+    user_input = st.text_input("Enter your name:")
+    
+    # Display a message based on user input
+    if user_input:
+        st.write(f"Hello, {user_input}!")
+
+if __name__ == "__main__":
+    main()
 
