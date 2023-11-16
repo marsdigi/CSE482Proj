862160f28db5b83f9ad9228f9ec6dd41b0d2dd52
marsdigi <81784993+marsdigi@users.noreply.github.com>
2023-11-15 22:41:24 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,107 @@
 # app.py
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
 import streamlit as st
 
 def main():
-    st.title("CSE-482 Project Application")
-    
-    # Add some text to the app
-    st.write("CSE-482 Project app!")
-
-    # Add a simple input widget
-    user_input = st.text_input("Enter a value:")
-    
-    # Display a message based on user input
-    if user_input:
-        st.write(f"The value, {user_input}!")
-
     st.header("Project: Automated Event Detection and Summarization for Reddit")
 
     st.subheader("1. Problem Definition")
@@ -85,3 +172,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+"""
