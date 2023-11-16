dab5a68608788f2b1145d53cb5f7fe9c9cf81866
marsdigi <81784993+marsdigi@users.noreply.github.com>
2023-11-15 21:07:43 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,11 +9,11 @@ def main():
     st.write("CSE-482 Project app!")
 
     # Add a simple input widget
-    user_input = st.text_input("Enter your name:")
+    user_input = st.text_input("Enter a value:")
     
     # Display a message based on user input
     if user_input:
-        st.write(f"Hello, {user_input}!")
+        st.write(f"The value, {user_input}!")
 
 if __name__ == "__main__":
     main()
